c2f6548a86f24f88c6b31058d290754ef2e7530a
bothropsinsularis <nishikinojeanne@gmail.com>
2024-11-29 16:31:11 -0300

    1

--- backend/login/login.php
+++ backend/login/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../frontend/public/css/style1.css">
+    <link rel="icon" href="../../frontend/public/imagens/recursos/logo.png">
     <title>Login</title>  
 </head>
 
